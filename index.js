d9ca58fc3b0ba930005c8aaef7a0cc5632dacd58
priscila <131805559+Rioske-Nakamura@users.noreply.github.com>
2024-06-06 15:24:22 -0400

    ssss

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const conn = require('./db/conn');
 const Usuario = require("./models/Usuario");
-const Jogo = require("./models/Jogo")
-const express = require("express")
-const app= express();
-const exphbs = require("express-handlebars");
+const Jogo = require("./models/Jogo");
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
 
 conn.authenticate()
     .then(() => {
@@ -19,67 +19,61 @@ conn.sync({ force: true })
         console.log("Banco de dados sincronizado com sucesso");
     })
     .catch((err) => {
-        console.log("Ocorreu um erro ao sincronizar/ conectar com o banco de dados: " + err);
+        console.log("Ocorreu um erro ao sincronizar/conectar com o banco de dados: " + err);
     });
 
-    app.engine("handlebars", exphbs.engine());
-    app.set("view engine", "handlebars");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
 app.use(
     express.urlencoded({
         extended: true
-        })
+    })
 );
 
 app.use(express.json());
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
-app.get("/usuarios", async (req,res)=>{
-    
-    const usuarios=await Usuario.findAll({raw: true});
-    res.render("usuarios", {usuarios});
-    
-})
+app.get("/usuarios", async (req, res) => {
+    try {
+        const usuarios = await Usuario.findAll({ raw: true });
+        res.render("usuarios", { usuarios });
+    } catch (err) {
+        res.status(500).send("Ocorreu um erro ao buscar os usuários");
+    }
+});
 
-app.get("/usuarios/novo", (req,res)=>{
+app.get("/usuarios/novo", (req, res) => {
     res.render("formUsuario");
-})
+});
 
-app.post("/usuarios/novo", async (req,res)=>{
-    const nickname= req.body.nickname;
-    const nome= req.body.nome;   
-    const dadosUsuario = {
-        nickname,
-        nome,
+app.post("/usuarios/novo", async (req, res) => {
+    const { nickname, nome } = req.body;
+    try {
+        const usuario = await Usuario.create({ nickname, nome });
+        res.send("Usuario inserido com o id: " + usuario.id);
+    } catch (err) {
+        res.status(500).send("Ocorreu um erro ao criar o usuário");
     }
-    const usuario = await Usuario.create(dadosUsuario)
+});
 
-    res.send("Usuario inserido sobre o id: " + usuario.id)
-})
-
-app.get("/jogos/novo", (req,res)=>{
+app.get("/jogos/novo", (req, res) => {
     res.render("formJogo");
-})
+});
 
-app.post("/jogos/novo", async (req,res)=>{
-    const titulo= req.body.titulo;
-    const descricao= req.body.descricao;
-    const precoBase= req.body.precoBase;
-    
-    const dadosJogo = {
-        titulo,
-        descricao,
-        precoBase,
+app.post("/jogos/novo", async (req, res) => {
+    const { titulo, descricao, precoBase } = req.body;
+    try {
+        const jogo = await Jogo.create({ titulo, descricao, precoBase });
+        res.send("O jogo inserido com o id: " + jogo.id);
+    } catch (err) {
+        res.status(500).send("Ocorreu um erro ao criar o jogo");
     }
-    const jogo = await Jogo.create(dadosJogo)
+});
 
-    res.send("O jogo inserido sobre o id: " + jogo.id)
-})
-
-app.listen(8000, ()=>{
-    console.log("O serve esta rodando na porta 8000!")
-})
-
+app.listen(8000, () => {
+    console.log("O servidor está rodando na porta 8000!");
+});
